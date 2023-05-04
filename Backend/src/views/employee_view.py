c8a57cd8dcb5d747ef82e8342f5dfbb28e4a64fb
Muhammad Arsalan <arcalankhan025@gmail.com>
2023-05-04 19:36:57 +0200

    V2

--- Backend/src/views/employee_view.py
+++ Backend/src/views/employee_view.py
@@ -3,7 +3,6 @@ logger = logging.getLogger(__name__)
 import io
 from flask import Flask, request, g, Blueprint, Response, json
 from source.models.employee_model import EmployeeModel
-
 muvid_api = Blueprint('muvid_api', __name__)
 
 # API Endpoints
